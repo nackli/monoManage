2e40ea616da59548fe32bfb86a5087d6096b5eb1
nackli <nackli@163.com>
2025-09-04 14:53:58 +0800

    删除build目录

--- build_aarch/CMakeFiles/monoDevCtrl_test.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/monoDevCtrl_test.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /root/work/monoManage/build_aarch/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /root/Tool/gcc-aarch64-linux-gnu-7.3/aarch64-linux-gnu/libc/usr/include/stdc-predef.h
